03241e9301045940996b3b3e7e19ad9fb5436ff6
artem <121548842+ArtemSavelyev731@users.noreply.github.com>
2025-11-04 18:00:14 +0300

    + package.json

---
+++ src/App.js
@@ -0,0 +1,12 @@
+﻿import React from "react";
+
+function App() {
+  return (
+    <div>
+      <h1>React приложение запущено! 🎉</h1>
+      <p>Теперь всё работает правильно.</p>
+    </div>
+  );
+}
+
+export default App;
